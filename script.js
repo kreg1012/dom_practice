801a6d16a9cef6d8af96395d79d7e046ebe37183
kreg1012 <kreg1012@gmail.com>
2022-11-18 17:45:13 -0500

    added 'innerContainer' div to script.js

--- script.js
+++ script.js
@@ -5,3 +5,30 @@ content.classList.add('content');
 content.textContent = 'this is the glorious textContent';
 
 container.appendChild(content);
+
+const p = document.createElement('p');
+p.classList.add('p');
+p.style.color = "red";
+p.textContent = "im a red P";
+
+content.appendChild(p);
+
+const h3 = document.createElement('h3');
+h3.classList.add('h3');
+h3.style.color = "blue";
+h3.textContent = "im a blue h3";
+
+content.appendChild(h3);
+
+const innerContainer = document.createElement('div');
+innerContainer.style.cssText = "border: 1 solid black; background-color: pink;";
+
+let innerContH1 = document.createElement('h1');
+innerContH1.textContent = "I'm in a DIV";
+
+let innerContP = document.createElement('p');
+innerContP.textContent = "ME TOO!";
+
+innerContainer.appendChild(innerContH1);
+innerContainer.appendChild(innerContP);
+container.appendChild(innerContainer);
